ca717a8f5ac9fb5971daae9eda08755097ede240
Dan Seethaler <dan@userlite.com>
2017-05-25 11:51:16 -0400

    Reorganized connect/disconnect process and added versioning

--- spec/userlite-link-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import UserliteLinkView from '../lib/userlite-link-view';
-
-describe('UserliteLinkView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
